daccfc14fa485d219e4a8a745655dbdab77003da
eva-erco <eva.ercolanelli@campusdavinci.edu.it>
2025-10-16 09:48:20 +0200

    Agg dell'ascissa e ordinata max e mix con le tuple

--- punti_cartesiano.py
+++ punti_cartesiano.py
@@ -14,27 +14,43 @@ for i in range(0,20):
 #scorrere la lista delle x, trovare il massimo e salvare l'indice
 #visualizzare poi con un print il numero
 massimo=x[0]
+indice_max=0
 
 for i in range(0,20):
     if x[i]>massimo:
         massimo=x[i]
+        indice_max=i
 print(massimo,y[massimo])    
 
 #ordinata minima ?
 #scorrere la lista delle y, trovare il minimo e salvare l'indice
 #visualizzare poi con un print il numero
 minimo=y[0]
+indice_min=0
 
 for i in range(0,20):
     if y[i]<minimo:
         minimo=y[i]
+        indice_min=i
 print(minimo,x[minimo])
 
 punti_cartesiano=[]
 for i in range(0,20):
     punto=(random.randint(0,10),random.randint(0,10))
     punti_cartesiano.append(punto)
-    
+ 
+massimo=punti_cartesiano[0]
+for i in range(1,20):
+     if punti_cartesiano[i][0]>massimo[0]:
+         massimo=punti_cartesiano[i]
+print(massimo) 
+
+minimo=punti_cartesiano[0]
+for i in range(1,20):
+     if punti_cartesiano[i][0]<minimo[0]:
+         minimo=punti_cartesiano[i]
+print(minimo)
+
 #come accedere alla x del primo punto
 print(punti_cartesiano[0][0])
 #come accedere alla y del primo punto
