a0f2f818e7ee7ae29bc707c310c57539ad4aca7d
eva-erco <eva.ercolanelli@campusdavinci.edu.it>
2026-02-11 19:34:56 +0100

    Add files via upload

--- esercizio_lista.py
+++ esercizio_lista.py
@@ -7,33 +7,48 @@
 #2) Media dei valori di una lista (FUNZIONE)
 #3) Calcolare la soglia [massimo della lista - 10] (FUNZIONE)
 #4) Contare il numero di valori sopra la soglia (PROCEDURA)
-import random 
+import random
+
 def creaLista(lista):
     n1=input("Inserire il primo estremo ")
     n1=int(n1)
-    n2=input("Iserire il secondo estremo ")
+    n2=input("Inserire il secondo estremo ")
     n2=int(n2)
-    for i in range (0,10):
-        if n1>n2:
-            valori=random.randint(n1,n2)
+    
+    for i in range(0,10):
+        if n1<n2:
+            valore=random.randint(n1,n2)
         else:
-            valori=random.randint(n2,n1)
-        lista.append(valori)
-        
+            valore=random.randint(n2,n1)
+        lista.append(valore)
+
 def calcoloMedia(lista):
     somma=0
     for i in range(0,len(lista)):
         somma=somma+lista[i]
     media=somma/len(lista)
     return(media)
-        
+
 def calcoloSoglia(lista):
-    max=
-    
-    
+    massimo=lista[0]
+    for i in range(1,len(lista)):
+        if lista[i]>massimo:
+            massimo=lista[i]
+    soglia=massimo-10
+    return(soglia)
+
+def contaSopraSoglia(lista,soglia):
+    contatore=0
+    for i in range(0,len(lista)):
+        if lista[i]>soglia:
+            contatore=contatore+1
+    return(contatore)
+
 if __name__=="__main__":
     listaM=[]
     creaLista(listaM)
-    mediValori=calcolo_media(listaM) 
-    
-    
+    mediaValori=calcoloMedia(listaM)
+    soglia=calcoloSoglia(listaM)
+    numeroSopra=contaSopraSoglia(listaM,soglia)
+    print(mediaValori)
+    print(numeroSopra)
